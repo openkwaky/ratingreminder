b6568a57cea7da455df09474213c70352633b2ca
Clément Quaquin <clemquaq@gmail.com>
2015-12-14 17:53:40 +0100

    Comments added in MainActivity

--- app/src/main/java/net/equasoft/testapplication/MainActivity.java
+++ app/src/main/java/net/equasoft/testapplication/MainActivity.java
@@ -17,15 +17,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Init RatingReminder object
         RatingReminder reminder = new RatingReminder(this);
+        // Give a name that will be used in dialogs for your app
         reminder.setAppName(getString(R.string.app_name));
+        //Select an algo, NO_GAP_ALOG triggers every time
         reminder.setAlgoType(AlgoType.NO_GAP_ALGO);
+        // Select a dialog type, basic dialog uses default system display
         reminder.setDialogType(RatingDialogType.BASIC_DIALOG);
 
+        // Configure a store
         ArrayList<StoreType> storeTypes = new ArrayList<>();
         storeTypes.add(StoreType.GOOGLE_PLAYSTORE);
         reminder.setStoreTypes(storeTypes);
 
+        // Trigger the dialog if algo decides to
         reminder.process();
 
     }
