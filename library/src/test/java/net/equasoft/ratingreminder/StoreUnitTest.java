41adad67ad5a14629d212e0eb8f7d2d9782b308e
Clément Quaquin <clemquaq@gmail.com>
2016-01-04 10:57:31 +0100

    add javadoc

---
+++ library/src/test/java/net/equasoft/ratingreminder/StoreUnitTest.java
@@ -0,0 +1,16 @@
+package net.equasoft.ratingreminder;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class StoreUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
